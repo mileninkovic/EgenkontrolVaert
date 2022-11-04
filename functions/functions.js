161a7fa5ba90d6bd3f0972817bf1797de7ea1c23
Mile Ninkovic <mile@ninkovic.dk>
2022-11-05 00:31:27 +0100

    Changed code structure

--- functions/functions.js
+++ functions/functions.js
@@ -2,11 +2,4 @@ const { app, BrowserWindow } = require("electron")
 
 module.exports = {
     exit: function(){app.quit()},
-    createBrowserWindow: function(){
-        const window = new BrowserWindow({
-            height: 200,
-            width: 200
-        });
-        window.loadFile("./view/index.html");
-    }
 }
--- main.js
+++ main.js
@@ -1,47 +1,73 @@
 const { app, BrowserWindow, Menu } = require('electron')
 const {exit, createBrowserWindow} = require('./functions/functions.js');
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-    })
+// const createWindow = () => {
+//     const win = new BrowserWindow({
+//       width: 800,
+//       height: 600,
+//     })
+//
+//     Menu.setApplicationMenu(menu);
+//     win.loadFile('view/index.html')
+// }
 
-    const menu = Menu.buildFromTemplate([
-        {
-            label: 'File',
-            submenu: [
-                {
-                    label:'New File',
-                    click() {
-                        createBrowserWindow();
-                        // Funktion eksisterer ikke!
-                    }
-                },
-                {
-                    label:'Save File',
-                    click() {
-                        // Funktion eksisterer ikke!
-                    }
-                },
-                {type:'separator'},
-                {
-                    label:'Exit',
-                    click() {
-                        exit();
-                    },
-                    accelerator: 'Alt+Q'
+const menu = Menu.buildFromTemplate([
+    {
+        label: 'Filer',
+        submenu: [
+            {
+                label:'Find kunde',
+                click() {
+                    createWindow("findclient.html", 600, 400);
+                    // Funktion eksisterer ikke!
                 }
-            ]
-        }
-    ])
+            },
+            {
+                label:'Indstillinger',
+                click() {
+                    // Funktion eksisterer ikke!
+                }
+            },
+            {type:'separator'},
+            {
+                label:'Exit',
+                click() {
+                    exit();
+                },
+                accelerator: 'Alt+Q'
+            }
+        ]
+    }
+])
+
+const windows = new Set();
+const createWindow = exports.createWindow = (viewName, width, height) => {
+    let newWindow = new BrowserWindow({ 
+        show: false,
+        width: width,
+        height: height, 
+    });
     
     Menu.setApplicationMenu(menu);
-    win.loadFile('view/index.html')
-}
+
+    newWindow.loadFile("view/" + viewName);
+  
+    newWindow.once('ready-to-show', () => {
+      newWindow.show();
+    });
+  
+    newWindow.on('closed', () => {
+      windows.delete(newWindow);
+      newWindow = null;
+    });
+  
+    windows.add(newWindow);
+    return newWindow;
+};
+  
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow("index.html", 800, 600)
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
